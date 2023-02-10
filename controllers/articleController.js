8972a5abfd4ce8bf7fb1dc789b1c7a37c2d82398
joaquin1990 <joaquingarese@gmail.com>
2023-02-10 10:16:16 -0300

    first commit

---
+++ controllers/articleController.js
@@ -0,0 +1,41 @@
+const { Article } = require("../models");
+
+// Display a listing of the resource.
+async function index(req, res) {
+  res.render("home");
+}
+
+// Display the specified resource.
+async function show(req, res) {
+  res.render("product");
+}
+
+// Show the form for creating a new resource
+async function create(req, res) {
+  res.render("create");
+}
+
+// Store a newly created resource in storage.
+async function store(req, res) {}
+
+// Show the form for editing the specified resource.
+async function edit(req, res) {}
+
+// Update the specified resource in storage.
+async function update(req, res) {}
+
+// Remove the specified resource from storage.
+async function destroy(req, res) {}
+
+// Otros handlers...
+// ...
+
+module.exports = {
+  index,
+  show,
+  create,
+  store,
+  edit,
+  update,
+  destroy,
+};
